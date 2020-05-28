d906e33733714a15dd8c30e1e3f05bf0efbd97a9
Allan Gaunt <allangaunt@gmail.com>
2020-05-28 17:02:02 +0100

    Update deps and use new modern build mode.

--- quasar.conf.js
+++ quasar.conf.js
@@ -72,6 +72,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
+      modern: true,
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // rtl: false, // https://quasar.dev/options/rtl-support
